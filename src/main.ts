71729abff017120a87a3c8622d665132543f4bf0
Scott Bedard <scottbedard90@gmail.com>
2023-06-21 00:03:06 -0500

    scaffold monaco

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
 import './style.css'
+
+import 'monaco-editor/esm/vs/editor/editor.all.js'
+import * as monaco from 'monaco-editor'
+
+const monacoEl = document.getElementById('monaco')
+
+if (!monacoEl) {
+  throw new Error('Monaco element not found')
+}
+
+monaco.editor.create(monacoEl, {
+  value: 'console.log(\'Hello, world 👋\')',
+  language: 'javascript',
+});
