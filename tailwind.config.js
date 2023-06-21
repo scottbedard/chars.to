05f3fabd09349d528c27a6674ede6bd446f57c48
Scott Bedard <scottbedard90@gmail.com>
2023-06-20 22:52:35 -0500

    scaffolding

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: '"Source Code Pro", ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+        sans: 'Quicksand, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      },
+    },
+  },
+  plugins: [],
+}
