aac3e8cdba271b04a32e8cdd17418c4db83d192a
Scott Bedard <scottbedard90@gmail.com>
2023-06-21 10:47:03 -0500

    basic styling

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,35 @@
 import './style.css'
-import 'monaco-editor/esm/vs/editor/editor.all.js'
 import * as monaco from 'monaco-editor'
+// // import 'monaco-editor/esm/vs/editor/editor.all.js'
+// // @ts-ignore
+// import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+// // @ts-ignore
+// import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
+// // @ts-ignore
+// import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
+// // @ts-ignore
+// import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
+// // @ts-ignore
+// import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+
+// // @ts-ignore
+// self.MonacoEnvironment = {
+//   getWorker(_: string, label: string) {
+//     if (label === 'json') {
+//       return new jsonWorker()
+//     }
+//     if (label === 'css' || label === 'scss' || label === 'less') {
+//       return new cssWorker()
+//     }
+//     if (label === 'html' || label === 'handlebars' || label === 'razor') {
+//       return new htmlWorker()
+//     }
+//     if (label === 'typescript' || label === 'javascript') {
+//       return new tsWorker()
+//     }
+//     return new editorWorker()
+//   },
+// }
 
 const monacoEl = document.getElementById('monaco')
 
@@ -20,8 +49,13 @@ try {
 } catch { }
 
 const editor = monaco.editor.create(monacoEl, {
+  fontFamily: '"Source Code Pro", ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+  fontSize: 16,
+  language: 'typescript',
+  lineDecorationsWidth: 0,
+  lineHeight: 24,
+  renderLineHighlight: 'none',
   value,
-  language: 'javascript',
 })
 
 document.addEventListener('keydown', e => {
